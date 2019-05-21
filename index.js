7c08a9fe4d6cdd06a0a9b291e8666e835642289c
kingmoc <joe.c.a.garcia@gmail.com>
2019-05-21 10:37:56 -0500

    Hamburger Animation Complete

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 function myFunction() {
 	var x = document.getElementById("myLinks");
-	if (x.style.display === "block") {
-	  x.style.display = "none";
-	} else {
-	  x.style.display = "block";
+
+	if(x.style.display === "block") {
+		
+		x.style.display = "none";
+		console.log('closing');
+	} 
+	else {
+		x.style.display = "block";
+		TweenMax.from(x, 1, {
+			y: -300,    
+			ease: Power4.easeOut,
+		});
+		
+		console.log('opening');
 	}
   }
