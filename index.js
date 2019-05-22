2ff3bf74b9b6887f459a06ec05118b5fb243ad14
kingmoc <joe.c.a.garcia@gmail.com>
2019-05-21 22:59:53 -0500

    Many Bugs Fixed

--- index.js
+++ index.js
@@ -1,20 +1,8 @@
 function myFunction() {
 	var x = document.getElementById("myLinks");
 
-	if(x.style.display === "block") {
-		
-		x.style.display = "none";
-		console.log('closing');
-	} 
-	else {
-		x.style.display = "block";
-		TweenMax.from(x, 1, {
-			y: -300,    
-			ease: Power4.easeOut,
-		});
-		
-		console.log('opening');
-	}
+		x.classList.toggle('active')
+
   }
 
 function buttonTextAnimation() {
@@ -30,4 +18,4 @@ function buttonTextAnimation() {
 	});
 }  
 
-buttonTextAnimation()
+// buttonTextAnimation()
