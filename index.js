9682521e4eb81c4510027d744bdf5f86ac687117
kingmoc <joe.c.a.garcia@gmail.com>
2019-05-23 13:27:53 -0500

    Scroll Top Complete

--- index.js
+++ index.js
@@ -19,3 +19,24 @@ function buttonTextAnimation() {
 }  
 
 // buttonTextAnimation()
+
+
+let up = document.querySelector('.far')
+
+//Hide Button 
+window.onscroll = () => scrollFunction();
+
+function scrollFunction() {
+	if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+		up.style.display = "block";
+	} 
+	  
+	else {
+		up.style.display = "none";
+	}
+}
+
+
+// Scroll Top
+up.addEventListener('click', () => 	document.documentElement.scrollTop = 0)
+
